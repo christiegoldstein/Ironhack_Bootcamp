ea3a1ba23c81f228a06e7c6276529db02ed63fbf
christiegoldstein <cngold15@gmail.com>
2016-06-14 13:49:54 -0400

    object methods using class Car

---
+++ Week_1/car.rb
@@ -0,0 +1,28 @@
+# Week_1/car.rb
+class Car
+	def initialize()
+		@list_of_cities = [] 
+	end
+
+	def make_noise
+		puts "BROOOOOM"
+	end
+
+	def speed_control 
+		puts "What is your current speed?"
+		speed = gets.chomp
+		if speed.to_i > 100
+			puts "AHH you're going over 100 mph!"
+		end
+	end
+
+	def travel(city)
+		puts "Traveling to: #{city}"
+		@list_of_cities.push(city)
+		#city must be an instance variable 
+	end 
+
+  	def list_of_cities
+   	    @list_of_cities
+  	end
+end
