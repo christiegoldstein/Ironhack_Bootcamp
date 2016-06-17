51ef65b8a68c28bf0f139a9c0b81ca8401810031
christiegoldstein <cngold15@gmail.com>
2016-06-17 11:55:03 -0400

    added queen regulations

--- Week_1/Day_5/Chess/lib/queen.rb
+++ Week_1/Day_5/Chess/lib/queen.rb
@@ -1,1 +1,24 @@
 #queen.rb
+
+class Queen
+	def initialize(x, y, color)
+		@x = x
+		@y = y
+		@color = color
+	end
+
+	def can_move(new_x, new_y)
+		
+		difference_x = @x - new_x
+		difference_y = @y - new_y
+
+		if  new_x == @x || new_y == @y
+			true
+		elsif difference_x.abs == difference_y.abs
+			true
+		else 
+			false
+		end
+
+	end
+end
