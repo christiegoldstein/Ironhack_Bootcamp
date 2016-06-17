33c2ff7f7451748c56fb376411f7d4560a8757ad
christiegoldstein <cngold15@gmail.com>
2016-06-17 10:58:51 -0400

    bishop class that hold restrictions of piece

--- Week_1/Day_5/Chess/lib/bishop.rb
+++ Week_1/Day_5/Chess/lib/bishop.rb
@@ -1,1 +1,29 @@
 #bishop.rb
+
+class Bishop
+	def initialize(x, y, color)
+		@x = x
+		@y = y
+		@color = color
+	end
+
+	def can_move(new_x, new_y)
+		number_of_moves = new_x - @x	
+		if(number_of_moves < 0)
+			number_of_moves = new_y - @y
+		end	
+
+		if(@color.downcase == "black" || (@x + number_of_moves == new_x && @y - number_of_moves == new_y))
+			true
+		elsif(@color.downcase == "black" || (@x - number_of_moves == new_x && @y + number_of_moves == new_y))
+			true
+		elsif(@color.downcase == "white" || (@x + number_of_moves == new_x && @y + number_of_moves == new_y))
+			true
+		elsif(@color.downcase == "white" || (@x - number_of_moves == new_x && @y + number_of_moves == new_y))
+			true
+		else 
+			false
+		end
+
+	end
+end
