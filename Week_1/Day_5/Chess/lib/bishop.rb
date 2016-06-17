1585a128afbb891aef6706ab192395f648da86d7
christiegoldstein <cngold15@gmail.com>
2016-06-17 11:25:46 -0400

    edited the black conditions for all to be true

--- Week_1/Day_5/Chess/lib/bishop.rb
+++ Week_1/Day_5/Chess/lib/bishop.rb
@@ -9,17 +9,14 @@ class Bishop
 
 	def can_move(new_x, new_y)
 		number_of_moves = new_x - @x	
-		if(number_of_moves < 0)
-			number_of_moves = new_y - @y
-		end	
 
-		if(@color.downcase == "black" || (@x + number_of_moves == new_x && @y - number_of_moves == new_y))
+		if(@color.downcase == "black" && (@x + number_of_moves.abs == new_x && @y - number_of_moves.abs == new_y))
 			true
-		elsif(@color.downcase == "black" || (@x - number_of_moves == new_x && @y + number_of_moves == new_y))
+		elsif(@color.downcase == "black" && (@x - number_of_moves.abs == new_x && @y - number_of_moves.abs == new_y))
 			true
-		elsif(@color.downcase == "white" || (@x + number_of_moves == new_x && @y + number_of_moves == new_y))
+		elsif(@color.downcase == "white" && (@x + number_of_moves.abs == new_x && @y + number_of_moves.abs == new_y))
 			true
-		elsif(@color.downcase == "white" || (@x - number_of_moves == new_x && @y + number_of_moves == new_y))
+		elsif(@color.downcase == "white" && (@x - number_of_moves.abs == new_x && @y + number_of_moves.abs == new_y))
 			true
 		else 
 			false
