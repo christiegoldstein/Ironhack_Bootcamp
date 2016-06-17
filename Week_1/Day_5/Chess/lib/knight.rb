ad96e2fa2e0807d47b5b7a242251432893eb9a11
christiegoldstein <cngold15@gmail.com>
2016-06-17 11:19:22 -0400

    knight class with restrictions of piece

--- Week_1/Day_5/Chess/lib/knight.rb
+++ Week_1/Day_5/Chess/lib/knight.rb
@@ -1,1 +1,28 @@
 #knight.rb
+
+#bishop.rb
+
+class Knight
+	def initialize(x, y, color)
+		@x = x
+		@y = y
+		@color = color
+	end
+
+	def can_move(new_x, new_y)
+		
+		black_y_diff = @y - new_y
+		white_y_diff = new_y - @y
+		puts black_y_diff
+		puts white_y_diff
+
+		if(@color.downcase == "black" && black_y_diff == 2 && (new_x == @x + 1 || new_x == @x -1))
+			true
+		elsif(@color.downcase == "white" && white_y_diff == 2 && (new_x == @x + 1 || new_x == @x -1))
+			true
+		else 
+			false
+		end
+
+	end
+end
