ba53b900929e092e59f71afce6b5092214188233
christiegoldstein <cngold15@gmail.com>
2016-06-17 10:39:37 -0400

    created king class to hold rules of what a king cannot do during the game

--- Week_1/Day_5/Chess/lib/king.rb
+++ Week_1/Day_5/Chess/lib/king.rb
@@ -1,1 +1,19 @@
 #king.rb
+class King
+	def initialize(x, y, color)
+		@x = x
+		@y = y
+		@color = color
+	end
+
+	def can_move(new_x, new_y)
+		if @x + 2 == new_x || @y + 2 == new_y
+			false
+		elsif @x - 2 == new_x || @y - 2 == new_y
+			false
+		else
+			true
+		end
+
+	end
+end
