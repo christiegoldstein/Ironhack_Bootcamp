7b00d6a115d9e28b0379b5af7dd1181bd7d5608b
christiegoldstein <cngold15@gmail.com>
2016-06-17 14:07:02 -0400

    created game class to create a game board

--- Week_1/Day_5/Chess/lib/pawn.rb
+++ Week_1/Day_5/Chess/lib/pawn.rb
@@ -1,1 +1,30 @@
 #pawn.rb
+class Pawn
+	def initialize(x, y, color)
+		@x = x
+		@y = y
+		@color = color
+	end
+
+	def can_move(new_x, new_y)
+		if(@color == "black")
+			if(@y == 7 && new_y - @y == -2 && @x == new_x)
+				true
+			elsif(@y < 7 && new_y - @y == -1 && @x == new_x)
+				true
+			else
+				false
+			end
+			
+		elsif(@color == "white")
+			if(@y == 2 && new_y - @y == 2 && @x == new_x)
+				true
+			elsif(@y > 2 && new_y - @y == 1 && @x == new_x)
+				true
+			else
+				false
+			end
+		end
+
+	end
+end
