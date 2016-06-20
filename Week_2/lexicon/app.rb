47a741be980fd97055ed3bf6d07082996c05aeab
christiegoldstein <cngold15@gmail.com>
2016-06-20 14:09:05 -0400

    lexicon folder for pair exercise

---
+++ Week_2/lexicon/app.rb
@@ -0,0 +1,5 @@
+#app.rb
+require_relative("lib/lexiconomitron.rb")
+
+ lexi = Lexiconomitron.new
+ lexi.eat_t("great scott!")
---
+++ Week_2/lexicon/lib/lexiconomitron.rb
@@ -0,0 +1,9 @@
+class Lexiconomitron
+	def eat_t(string)
+		str = string.split(//)
+		p str
+		str.delete("t")
+		p str
+		p str.join
+	end
+end
