1997e3294588929b24452dc79f47cd64e6c08b7a
christiegoldstein <cngold15@gmail.com>
2016-06-20 10:51:24 -0400

    comment out print statements

--- Week_2/Day_1/unit_testing/lib/string_calculator.rb
+++ Week_2/Day_1/unit_testing/lib/string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
 	def add(numbers_string)
 		numbers_arr = numbers_string.split(/[\s,&]/)
 	
-		p numbers_arr
+		#p numbers_arr
 		sum = 0 
 		numbers_arr.each do |number|
 			sum = sum + number.to_i
