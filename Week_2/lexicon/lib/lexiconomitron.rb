bf88db68ce141a6c4ee3c962195c6c3373301a48
christiegoldstein <cngold15@gmail.com>
2016-06-20 14:47:25 -0400

    added a shazam method to reverse each word in array

--- Week_2/lexicon/lib/lexiconomitron.rb
+++ Week_2/lexicon/lib/lexiconomitron.rb
@@ -1,9 +1,21 @@
 class Lexiconomitron
 	def eat_t(string)
 		str = string.split(//)
-		p str
-		str.delete("t")
-		p str
-		p str.join
+		t_letter = "T"
+		str.delete(t_letter.downcase)
+		str.delete(t_letter)
+		str
+		str.join
+	end
+
+	def shazam(arr)
+		arr.each do |word|
+			word.reverse!
+		end
+	
+		new_arr = [arr[0],arr[arr.length-1]]
+		
+		eat_arr = eat_t(new_arr.join(","))
+		eat_arr.split(",")
 	end
 end
