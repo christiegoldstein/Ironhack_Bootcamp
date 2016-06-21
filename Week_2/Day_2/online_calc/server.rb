39c8c9bb17056b7083187b9d5e94f5b5b9d07683
christiegoldstein <cngold15@gmail.com>
2016-06-21 18:21:44 -0400

    changed to match home.erb class

--- Week_2/Day_2/online_calc/server.rb
+++ Week_2/Day_2/online_calc/server.rb
@@ -1,6 +1,75 @@
 # server.rb
-require "sinatra"
 
-get "/add" do
-  erb(:add)
+require "sinatra"
+require_relative("lib/calculator.rb")
+
+calculator = Calculator.new
+
+get "/" do
+	erb(:home)
 end
+
+post "/operate" do
+	@first = params[:first_number].to_f
+  	@second = params[:second_number].to_f
+  	value = params[:operation]
+
+  	if(value == "add")
+  		calculator.add(@first, @second).to_s
+  	elsif(value == "subtract")
+  		calculator.subtract(@first, @second).to_s
+  	elsif(value == "multiply")
+  		calculator.multiply(@first, @second).to_s
+  	elsif(value == "divide")
+  		calculator.divide(@first, @second).to_s
+  	end
+end
+
+
+
+
+
+# get "/add" do
+#   erb(:add)
+# end
+
+# get "/subtract" do
+#   erb(:subtract)
+# end
+
+# get "/multiply" do
+#   erb(:multiply)
+# end
+
+# get "/divide" do
+#   erb(:divide)
+# end
+
+# post "/calculate_add" do
+
+#   @first = params[:first_number].to_f
+#   @second = params[:second_number].to_f
+#   calculator.add(@first, @second).to_s
+
+# end
+
+# post "/calculate_subtract" do
+
+#   @first = params[:first_number].to_f
+#   @second = params[:second_number].to_f
+#   calculator.subtract(@first, @second).to_s
+# end
+
+# post "/calculate_multiply" do
+
+#   @first = params[:first_number].to_f
+#   @second = params[:second_number].to_f
+#   calculator.multiply(@first, @second).to_s
+# end
+
+# post "/calculate_divide" do
+
+#   @first = params[:first_number].to_f
+#   @second = params[:second_number].to_f
+#   calculator.divide(@first, @second).to_s
+# end
