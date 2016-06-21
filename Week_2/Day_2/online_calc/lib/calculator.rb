e37efbd8e396be0a530681cba852e879b4da1c1a
christiegoldstein <cngold15@gmail.com>
2016-06-21 18:04:04 -0400

    calculator class for math functions

---
+++ Week_2/Day_2/online_calc/lib/calculator.rb
@@ -0,0 +1,23 @@
+class Calculator
+	
+	def add(number1, number2)
+		result = number1 + number2
+
+	end
+
+	def subtract(number1, number2)
+		result = number1 - number2
+		result
+	end
+
+	def multiply(number1, number2)
+		result = number1 * number2
+		result
+	end
+
+	def divide(number1, number2)
+		result  = number1 / number2
+		result
+	end
+
+end
