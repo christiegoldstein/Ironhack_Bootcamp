7b714caac101472f606bfc55a562a81695062728
christiegoldstein <cngold15@gmail.com>
2016-06-21 15:27:07 -0400

    give an optional parameter for font

--- Week_2/Day_2/ascii_word/ascii.rb
+++ Week_2/Day_2/ascii_word/ascii.rb
@@ -1,9 +1,17 @@
 require "sinatra"
 require "artii"
 
-get "/ascii/:word" do
-	word = params[:word]
-	ascifier = Artii::Base.new(:font => "doh")
-	@ascii_word = ascifier.asciify(word)
-	erb(:ascii_page)
+get "/ascii/:word/?:font?" do
+	#"Hello #{params[:word]}, #{params[:font]}"
+	@word = params[:word]
+	@font = params[:font]
+	if @font == nil
+		ascifier = Artii::Base.new(:font => "doh")
+		@ascii_word = ascifier.asciify(@word)
+		erb(:ascii_page)
+	elsif @font == "alligator" || @font == "acrobatic" || @font == "avatar"
+		ascifier = Artii::Base.new(:font => @font)
+		@ascii_word = ascifier.asciify(@word)
+		erb(:font_ascii_page)
+	end
 end
