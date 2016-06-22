97be34d288dd1896ff9d49ef4a6d8b273b1bfbab
christiegoldstein <cngold15@gmail.com>
2016-06-22 09:55:04 -0400

    created a post to save to memory

--- Week_2/Day_2/online_calc/server.rb
+++ Week_2/Day_2/online_calc/server.rb
@@ -2,30 +2,42 @@
 
 require "sinatra"
 require_relative("lib/calculator.rb")
+require_relative("lib/memorysaver.rb")
+
 
 calculator = Calculator.new
+my_memory = MemorySaver.new
 
 get "/" do
+	@memory_number = my_memory.load
 	erb(:home)
+
 end
 
 post "/operate" do
+
 	@first = params[:first_number].to_f
   	@second = params[:second_number].to_f
-  	value = params[:operation]
+  	@value = params[:operation]
 
-  	if(value == "add")
-  		calculator.add(@first, @second).to_s
-  	elsif(value == "subtract")
-  		calculator.subtract(@first, @second).to_s
-  	elsif(value == "multiply")
-  		calculator.multiply(@first, @second).to_s
-  	elsif(value == "divide")
-  		calculator.divide(@first, @second).to_s
-  	end
+	if(@value == "add")
+		@final = calculator.add(@first, @second).to_s
+	elsif(@value == "subtract")
+	  	@final = calculator.subtract(@first, @second).to_s
+	elsif(@value == "multiply")
+	  	@final = calculator.multiply(@first, @second).to_s
+	elsif(@value == "divide")
+	  	@final = calculator.divide(@first, @second).to_s
+	end
+
+	erb(:result)
+
 end
 
-
+post "/save_to_memory" do
+	my_memory.save(params[:result])
+	redirect to("/")
+end
 
 
 
