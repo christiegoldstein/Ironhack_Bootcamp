3ce439a6b92527a5022cf7bd1c1aa7490c435246
christiegoldstein <cngold15@gmail.com>
2016-06-29 10:41:45 -0400

    created a color counter

--- Week_3/Day_3/jsprac/file.js
+++ Week_3/Day_3/jsprac/file.js
@@ -1,7 +1,18 @@
 var colorsArray = ["black", "purple", "black", "chartreuse", "indigo", "magenta", "scarlet", "indigo", "indigo",];
 
-function countColors (){
-    
+function countColors (color, the_array){
+
+	var colorOccurances = 0;
+
+	for(var counter = 0; counter < the_array.length; counter++){
+		var theColor = the_array[counter]
+
+		if(theColor == color){
+			colorOccurances++;
+		}
+	}
+
+	return colorOccurances;  
 }
 
 console.log("testing colors function")
