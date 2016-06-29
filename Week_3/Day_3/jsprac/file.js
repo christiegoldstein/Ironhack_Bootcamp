1940eae224044badecb53fd182949666db562788
christiegoldstein <cngold15@gmail.com>
2016-06-29 10:24:21 -0400

    day 3 work with javascript

---
+++ Week_3/Day_3/jsprac/file.js
@@ -0,0 +1,13 @@
+var colorsArray = ["black", "purple", "black", "chartreuse", "indigo", "magenta", "scarlet", "indigo", "indigo",];
+
+function countColors (){
+    
+}
+
+console.log("testing colors function")
+
+console.log(countColors("indigo", colorsArray) === 3);
+
+console.log(countColors("scarlet", colorsArray) === 1);
+
+console.log(countColors("pink", colorsArray) === 0);
