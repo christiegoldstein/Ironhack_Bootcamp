fd5d7786ee12b3aac8d709f01718144b71dd1dc1
christiegoldstein <cngold15@gmail.com>
2016-06-30 14:48:39 -0400

    files for vikings exercise

---
+++ Week_3/Day_4/vikings/lib/saxon.js
@@ -0,0 +1,8 @@
+"use strict";
+
+class Saxon{
+	constructor(health, strength){
+		this.health = health;
+		this.strength = strength;
+	}
+}
---
+++ Week_3/Day_4/vikings/lib/vikings.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class Viking{
+	constructor(name, health, strength){
+		this.name = name;
+		this.health = health;
+		this.strength = strength;
+	}
+}
