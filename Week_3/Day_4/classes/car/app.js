bf93fd5dbb0d5ca563d056624c60c41b9b956464
christiegoldstein <cngold15@gmail.com>
2016-06-30 10:24:46 -0400

    created car class in js

---
+++ Week_3/Day_4/classes/car/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var Car = require("./lib/car.js");
+
+
+var bmw = new Animal("BMW", "Vrooooom");
---
+++ Week_3/Day_4/classes/car/lib/car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor (model, noise) {
+    this.model = model;
+    this.noise = noise;
+    this.num_of_wheels = 4;
+  }
+
+  makeNoise () {
+    console.log(this.noise + "!!!!");
+  }
+}
+
+module.exports = Car;
