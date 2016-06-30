c23a20e2a239d3f4a826d3fb8773feb3a23276a9
christiegoldstein <cngold15@gmail.com>
2016-06-30 10:03:28 -0400

    newer an better way of creating classes in JS

--- Week_3/Day_4/classes/app.js
+++ Week_3/Day_4/classes/app.js
@@ -1,13 +1,27 @@
-function Animal(name, noise){
-	this.name = name;
-	this.noise = noise;
 
-};
+// //clean and newer way of creating a class in JS
+// class Animal{
+// 	constructor(name, noise){
+// 		this.name = name;
+// 		this.noise = noise;
+// 	}
+
+// 	shout(){
+// 		console.log("Moooo!");
+// 	}
+// }
+
+// //old way of creating classes
+// function Animal(name, noise){
+// 	this.name = name;
+// 	this.noise = noise;
+
+// };
 
 
-Animal.prototype.shout: funtion(){
-		console.log("Moooo!");
-};
+// Animal.prototype.shout: funtion(){
+// 		console.log("Moooo!");
+// };
 
 var shadow = new Animal("Shadow", "Ruff");
 console.log(shadow.name);
