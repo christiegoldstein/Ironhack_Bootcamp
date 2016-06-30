c192bfa8f612315bc397a70103ee74bbefd5fd32
christiegoldstein <cngold15@gmail.com>
2016-06-30 12:28:01 -0400

    added file reader syntax

--- Week_3/Day_4/classes/car/app.js
+++ Week_3/Day_4/classes/car/app.js
@@ -1,7 +1,12 @@
 "use strict";
 
+var fs = require("fs");
 var Car = require("./lib/car.js");
 
+fs.readFile("movies.txt", "utf8", function(theError, fileContents){
+	console.log("\nFile has been read: Printing it's contents:")
+	console.log(fileContents);
+});
 
 var bmw = new Car("BMW", "Vrooooom");
 bmw.makeNoise();
