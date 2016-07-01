e21e43fcea4b8851b6fd7d5ff8ce661f110ba454
christiegoldstein <cngold15@gmail.com>
2016-07-01 12:35:27 -0400

    made a random question a bonus question worth double

--- Week_3/Day_5/quiz/lib/quiz.js
+++ Week_3/Day_5/quiz/lib/quiz.js
@@ -20,6 +20,8 @@ class Quiz{
 	}
 
 	callBack(error, input){
+		var randomnumber = Math.floor(Math.random()*this.quiz_arr.length);
+
 		if(error){
 			console.log(error.message);
 		}
@@ -30,8 +32,15 @@ class Quiz{
 			console.log("Correct!");
 			// console.log(this.quiz_arr[this.i].points);
 			this.i++;
-			this.total += this.quiz_arr[this.i].points;
-			console.log(this.total);
+			if(this.quiz_arr[this.i].id === randomnumber){
+				this.total+= 20;
+				console.log("You got the bonus!");
+				console.log(this.total);
+			}
+			else if(this.quiz_arr[this.i].id !== randomnumber){
+				this.total += this.quiz_arr[this.i].points;
+				console.log(this.total);
+			}
 			this.take_quiz();
 		}		
 		else if(this.quiz_arr[this.i].answer !== input){
