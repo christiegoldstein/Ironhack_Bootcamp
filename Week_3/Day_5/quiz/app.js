642011d6cc1733fd28cb8e4cdf0faa5bc0baf5f0
christiegoldstein <cngold15@gmail.com>
2016-07-01 11:56:52 -0400

    added all questions to the quiz class

---
+++ Week_3/Day_5/quiz/app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var read = require("read");
+var Question = require("./lib/question.js");
+var Quiz = require("./lib/quiz.js");
+
+var friends_quiz = new Quiz();
+
+console.log("Welcome to Friend's Trivia Quiz! Let's see how well you know the show!");
+
+var question1 = new Question("What soap opera did Joey act in?", "Days of Our Lives", 1);
+friends_quiz.add_question(question1);
+var question2 = new Question("What was the name of the song Phoebe sang?", "Smelly Cat", 2);
+friends_quiz.add_question(question2);
+var question3 = new Question("What was Rachel's almost husband-to-be's name?", "Barry", 3);
+friends_quiz.add_question(question3);
+var question4 = new Question("Which band sings the iconic theme song", "The Remebrandts", 4);
+friends_quiz.add_question(question4);
+var question5 = new Question("What's Monica's apartment number?", "20", 5);
+friends_quiz.add_question(question5);
+var question6 = new Question("What was the name of Joey's reclining chair?", "Rosita", 6);
+friends_quiz.add_question(question6);
+var question7 = new Question("What name did Phoebe use when pretending to be someone else?", "Regina Phalange", 7);
+friends_quiz.add_question(question7);
+var question8 = new Question("How many times was Ross married?", "3", 8);
+friends_quiz.add_question(question8);
+var question9 = new Question("What did Rachel name her daughter?", "Emma", 9);
+friends_quiz.add_question(question9);
+var question10 = new Question("Who did Phoebe mug on the streets?", "Ross", 10);
+friends_quiz.add_question(question10);
+
+friends_quiz.take_quiz();
