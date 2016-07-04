f1e4b73c42b2ecfb8e6e390e56d62f53c2ee2e81
christiegoldstein <cngold15@gmail.com>
2016-07-04 14:25:04 -0400

    created text form and received hardcoded text when submitted

--- Week_4/word_central/app/controllers/text_inspections_controller.rb
+++ Week_4/word_central/app/controllers/text_inspections_controller.rb
@@ -2,4 +2,9 @@ class TextInspectionsController < ApplicationController
 	def new 
 		render 'new'
 	end
+
+	# ***** New *****
+  	def create
+   		render plain: "This route is text_inspections#create."
+  	end
 end
--- Week_4/word_central/config/routes.rb
+++ Week_4/word_central/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 	get '/', to: 'site#home'
 
 	get "/text_inspections/new", to: 'text_inspections#new'
+	
+	# ***** New *****
+  	post "/text_inspections", to: "text_inspections#create"
 end
