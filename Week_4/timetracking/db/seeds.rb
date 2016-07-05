db1c463b3b8fdbe23f3103cbb2ad9992088cfac8
christiegoldstein <cngold15@gmail.com>
2016-07-05 14:40:23 -0400

    created seed to hold ten projects and detailed what was in database and added project description to view

--- Week_4/timetracking/db/seeds.rb
+++ Week_4/timetracking/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+projectA = Project.create(name: 'Ironhack', description: 'Ironhack is a coding school')
+projectB = Project.create(name: 'Pape Johns', description: 'Decent chain for pizza')
+projectC = Project.create(name: 'Taco Bell')
+projectD = Project.create(name: 'Dominos', description: 'I like their breadsticks')
+projectE = Project.create(name: 'Meal Pass', description: 'Great way to get lunch around Miami')
+projectF = Project.create(name: 'Blue Tree', description: 'Has a refreshing acai bowl')
+projectG = Project.create(name: 'Japango', description: 'I want their tuna tartar roll for dinner')
+projectH = Project.create(name: 'Shula Burger', description: 'Had a burger from there last night')
+projectI = Project.create(name: 'Freshmarket')
+projectJ = Project.create(name: 'Toasted')
+
+puts 'done'
