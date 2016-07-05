a32b968a25a85e05e9da52b5e49597c5e42e7fec
christiegoldstein <cngold15@gmail.com>
2016-07-05 16:48:56 -0400

    created new contact and added it to original list

--- Week_4/Day_2/address_book/app/controllers/contacts_controller.rb
+++ Week_4/Day_2/address_book/app/controllers/contacts_controller.rb
@@ -1,6 +1,24 @@
 class ContactsController < ApplicationController
 	def index
 		@contacts_array = Contact.all
-		render 'index'
+		render "index"
 	end
+
+	def new
+		render "new"
+	end
+
+	def create
+    	contact = Contact.new(
+      	:name => params[:contact][:name],
+      	:address => params[:contact][:address],
+      	:number => params[:contact][:number],
+      	:email => params[:contact][:email])
+
+    	# Now we save the contact
+   	 	contact.save
+
+    	redirect_to  ("/")
+   		
+  	end
 end
--- Week_4/Day_2/address_book/config/routes.rb
+++ Week_4/Day_2/address_book/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
-	get '/', to:'contacts#index'
+	get "/", to: "contacts#index"
+
+	get "/contacts/new", to: "contacts#new"
+
+	post("/contacts"  => "contacts#create")
+
+
 end
