f98500365829cfa409ec4750f2b1f8e5b432bd03
christiegoldstein <cngold15@gmail.com>
2016-07-06 10:12:43 -0400

    used show action to make a page with specific project and description

--- Week_4/timetracking/app/controllers/projects_controller.rb
+++ Week_4/timetracking/app/controllers/projects_controller.rb
@@ -2,6 +2,13 @@ class ProjectsController < ApplicationController
 	def index
 		@projects_array = Project.order(created_at: "desc")
 								 .limit(10)
-	render 'index'
+		render 'index'
+	end
+
+	def show
+
+		@project = Project.find(params[:id])
+
+		render 'show'
 	end
 end
