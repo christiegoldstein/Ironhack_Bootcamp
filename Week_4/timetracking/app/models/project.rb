eb5a0669a43233fe1451b8eb87a22ed3e9ef0d25
christiegoldstein <cngold15@gmail.com>
2016-07-06 12:05:15 -0400

    added comments to show what you do in rails console you can aslo hardcode in model

--- Week_4/timetracking/app/models/project.rb
+++ Week_4/timetracking/app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ApplicationRecord
+	has_many :time_entries
 end
+
+
+#the_project = Project.find_by(id: 1)
+#the_project.time_entries
+
+
+#this is the same as 
+#TimeEntry.where(project_id: 1)
