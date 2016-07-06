c4da58971020b2c82baabfe38ddf7ca4d85b4b4b
christiegoldstein <cngold15@gmail.com>
2016-07-06 11:07:58 -0400

    link on home page to add a project then redirect back once new project is created

--- Week_4/timetracking/app/controllers/projects_controller.rb
+++ Week_4/timetracking/app/controllers/projects_controller.rb
@@ -8,11 +8,25 @@ class ProjectsController < ApplicationController
 	def show
 
 		@project = Project.find(params[:id])
-		
+
 		unless @project
 			render 'no_projects_found'
 		end
 
 		render 'show'
+	end
+
+	def new
+		@my_project = Project.new
+		render 'new'
+	end
+
+	def create
+		@my_project = Project.new(
+			:name =>  params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+
+		redirect_to "/projects"
 	end
 end
