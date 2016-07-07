503750b4c572eb0f8dcc8c1b2662b67e504753ba
christiegoldstein <cngold15@gmail.com>
2016-07-07 12:33:09 -0400

    can edit exisiting entries

--- Week_4/timetracking/app/controllers/time_entries_controller.rb
+++ Week_4/timetracking/app/controllers/time_entries_controller.rb
@@ -21,9 +21,31 @@ class TimeEntriesController < ApplicationController
 			:date => params[:time_entry][:date])
 
 		if @my_entry.save
-			redirect_to '/projects/#{@my_project.id}/time_entries'
+			redirect_to action: 'index', controller: 'time_entries',
+						project_id: @my_project.id
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.find(params[:id])
+		render 'edit'
+	end
+
+	def update
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.find(params[:id])
+
+		if @my_entry.update(hours: params[:time_entry][:hours],
+						minutes: params[:time_entry][:minutes],
+						comments: params[:time_entry][:comments],
+						date: params[:time_entry][:date])
+			redirect_to action: 'index', controller: 'time_entries',
+						project_id: @my_project.id
+		else
+			render 'edit'
 		end
 	end
 
--- Week_4/timetracking/config/routes.rb
+++ Week_4/timetracking/config/routes.rb
@@ -11,10 +11,14 @@ Rails.application.routes.draw do
 
 	post '/projects', to: 'projects#create'
 
-	get '/projects/:project_id/time_entries', to: 'projects#index'
+	get '/projects/:project_id/time_entries', to: 'time_entries#index'
 
 	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
 
+	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
+
 	post '/projects/:project_id/time_entries',
 		to: 'time_entries#create', as: :project_time_entries
+
+	patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: "project_time_entry"
 end
