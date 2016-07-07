587335656ac5bcec6253b303dca28e55271ea49d
christiegoldstein <cngold15@gmail.com>
2016-07-07 10:54:22 -0400

    made a method to destroy projects older than 1 week

--- Week_4/timetracking/app/models/project.rb
+++ Week_4/timetracking/app/models/project.rb
@@ -1,5 +1,12 @@
 class Project < ApplicationRecord
 	has_many :time_entries
+
+	def self.clean_old
+		#find projects older than a week
+		#destroy them
+		Project.where("created_at < ?", Time.now - 7.days).destroy_all
+
+	end
 end
 
 
