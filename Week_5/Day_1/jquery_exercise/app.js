baae1602e90abe7bacd70aad1554b169bf00ae31
christiegoldstein <cngold15@gmail.com>
2016-07-11 18:16:29 -0400

    pressing enter and submitting a form to a new phrases to array

--- Week_5/Day_1/jquery_exercise/app.js
+++ Week_5/Day_1/jquery_exercise/app.js
@@ -10,5 +10,21 @@ $(document).on('ready',function(){
 		$ ('.phrase').html('<p>'+ phrases[Math.floor(Math.random() * phrases.length)] +'</p>');
 	});
 
+	// $("input").keypress(function(event) {
+	//     if (event.which == 13) {
+	//     	phrases.push($('input').val());
+	//         console.log(phrases);
+
+	//         //event.preventDefault();
+	//        // $("form").submit();
+	//     }
+	// });
+
+	$("form").submit(function() {
+		phrases.push($('input').val());
+	    console.log(phrases); 
+		return false; 
+	});
+
 });
 
