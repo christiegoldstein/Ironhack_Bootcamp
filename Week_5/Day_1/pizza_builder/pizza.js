4f31c62f0f644e625ed21d55b3a114307f241b2e
christiegoldstein <cngold15@gmail.com>
2016-07-11 15:25:35 -0400

    making a pizza builder using jquery

---
+++ Week_5/Day_1/pizza_builder/pizza.js
@@ -0,0 +1,19 @@
+// Write your Pizza Builder JavaScript in this file.
+
+$ (document).on('ready',function(){
+
+	$ ('.btn-pepperonni').on('click',function(){
+		$ ('.pep').toggle();
+	});
+
+
+	$ ('.btn-mushrooms').on('click',function(){
+		$ ('.mushroom').toggle();
+	});
+
+
+	$ ('.btn-green-peppers').on('click',function(){
+		$ ('.green-pepper').toggle();
+	});
+
+});
