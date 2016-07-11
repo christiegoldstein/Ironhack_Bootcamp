5b8b96c9ec83098b3ce2d8d17ff763d0a266244e
christiegoldstein <cngold15@gmail.com>
2016-07-11 14:05:32 -0400

    updated all routes

--- Week_4/timetracking/config/routes.rb
+++ Week_4/timetracking/config/routes.rb
@@ -1,32 +1,12 @@
 Rails.application.routes.draw do
+
 	get '/', to: 'site#home'
 
 	get '/contact', to: 'site#contact' 
 
-	#Project Routes
 
-	get '/projects', to: 'projects#index'
-
-	get '/projects/new', to: 'projects#new'
-
-	get '/projects/:id', to: 'projects#show'
-
-	post '/projects', to: 'projects#create'
-
-	#Time entries routes
-
-	get '/projects/:project_id/time_entries', to: 'time_entries#index'
-
-	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
-
-	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
-
-	post '/projects/:project_id/time_entries',
-		to: 'time_entries#create', as: :project_time_entries
-
-	patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: "project_time_entry"
-
-	delete 'projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
-
+	resources :projects, only: [:index, :show, :new, :create] do
+		resources :time_entries, except: [:show]
+	end
 
 end
