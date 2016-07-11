30a6928e31d8d579a425e484fa60849be60fa04f
christiegoldstein <cngold15@gmail.com>
2016-07-11 15:36:29 -0400

    made buttos active when clicked on

--- Week_5/Day_1/pizza_builder/pizza.js
+++ Week_5/Day_1/pizza_builder/pizza.js
@@ -4,24 +4,29 @@ $ (document).on('ready',function(){
 
 	$ ('.btn-pepperonni').on('click',function(){
 		$ ('.pep').toggle();
+		$ (this).toggleClass("active");
 	});
 
 
 	$ ('.btn-mushrooms').on('click',function(){
 		$ ('.mushroom').toggle();
+		$ (this).toggleClass("active");
 	});
 
 
 	$ ('.btn-green-peppers').on('click',function(){
 		$ ('.green-pepper').toggle();
+		$ (this).toggleClass("active");
 	});
 
 	$ ('.btn-sauce').on('click',function(){
 		$ ('.sauce').toggleClass('sauce-white');
+		$ (this).toggleClass("active");
 	});
 
 	$ ('.btn-crust').on('click',function(){
 		$ ('.crust').toggleClass('crust-gluten-free');
+		$ (this).toggleClass("active");
 	});
 
 });
