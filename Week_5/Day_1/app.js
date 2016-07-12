944040987e66427cc954ce4a7bbc1496e9038fae
christiegoldstein <cngold15@gmail.com>
2016-07-12 16:43:07 -0400

    added a character created in form to the list of exisiting characters in ironhack characters api

--- Week_5/Day_1/app.js
+++ Week_5/Day_1/app.js
@@ -1,12 +1,45 @@
 $(document).on('ready',function(){
+	$('form').hide();
+	$('.list-button').on('click',function(event){
+		event.preventDefault();
 
-	$('.js-button').on('click',function(){
-		$ ('.to_fade').fadeOut();
+		$.ajax({
+			url: 'https://ironhack-characters.herokuapp.com/characters',
+			success: showCharacters
+		});
+
+		$('.list-button').hide();
+		$('form').show();
+		form();
 	});
 
-	$('.unicorn-button').on('click',function(){
-		$ ('.to_fade').append('<p>And I like Unicorns!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>');
-		$ ('body').addClass('rainbow');
-	});
+	function showCharacters(charactersArray){
+          charactersArray.forEach(function(theCharacter){
+            var html = '<p>' + theCharacter.name + '</p>';
+            $('ul').append(html);
+  		  });
+  	}
+
+  	function form(){
+		$(".js-character-form").on("submit", function(event){
+		 	event.preventDefault();
+		 	var new_char_arr = [ {
+		 		name: $('.name').val(),
+		 		occupation: $('.occupation').val(),
+		 		weapon: $('.weapon').val()
+		 	}];
+
+			showCharacters(new_char_arr);
+		 });
+	}
+
+	// $('.js-button').on('click',function(){
+	// 	$ ('.to_fade').fadeOut();
+	// });
+
+	// $('.unicorn-button').on('click',function(){
+	// 	$ ('.to_fade').append('<p>And I like Unicorns!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>');
+	// 	$ ('body').addClass('rainbow');
+	// });
 
 });
