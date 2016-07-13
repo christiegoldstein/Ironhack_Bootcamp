b8737e48425664bc30a4f6925512c3bca5b807c2
christiegoldstein <cngold15@gmail.com>
2016-07-13 16:35:09 -0400

    can click on artist and will display modal with all album title and images

--- Week_5/Day_2/Spotify/app.js
+++ Week_5/Day_2/Spotify/app.js
@@ -8,26 +8,63 @@ $(document).on('ready',function(){
 		var link = 'https://api.spotify.com/v1/search?type=artist&query='+artist_name;
 
 		$.ajax({
+			type: "GET",
 			url: link,
-			success: function(response){
-				showArtist(response)
-
-			}
+			success: showArtist
 
 		});
 		
 	});
 
 	function showArtist(artist){
-		for(var i = 0; i < artist.artists.items.length; i++){
-			$(".artist_info").append(artist.artists.items[i].name)
+		//artist.artists.items.length
+		$(".artist_info").empty();
+		for(var i = 0; i < 1; i++){
+			$(".artist_info").append("<h3>"+artist.artists.items[i].name+"</h3>");
+			$(".artist_info").append(`<a href='#' data=${artist.artists.items[i].id}>Click here to see albums</a>`);
 			if(artist.artists.items[i].images[0] != null){
-				$(".artist_info").append('<img src='+artist.artists.items[i].images[0].url+'><br/>');
+				$(".artist_info").append('<img src='+artist.artists.items[i].images[1].url+'>');
 			}
 		}
-		//$(".container").append(artist.artists.images[0]);
 
-		//console.log(artist);
+		$("a").on("click", function(event){
+			event.preventDefault();
+		    $(".modal").modal("show");
+
+			var id = artist.artists.items[0].id;
+
+			console.log(id);
+
+			var album_link = 'https://api.spotify.com/v1/artists/'+id+'/albums';
+
+			 $.ajax({
+			 	type: "GET",
+			 	url: album_link,
+			 	success: showAlbum
+			 });
+		});
+
+	}
+
+	function showAlbum(albums){
+		$(".modal-body").empty();
+		for(var i = 0; i < albums.items.length; i++){
+			if(albums.items[i].name != null && albums.items[i].images[1].url != null){
+				var album_name = albums.items[i].name;
+				var album_img = albums.items[i].images[1].url;
+				console.log(album_name);
+				console.log(album_img);
+				var album_list = `
+					<ul>
+						<li>	
+							<h4>${album_name}</h4>
+							<img src=${album_img}>
+						</li>
+					</ul>
+				`;
+				$(".modal-body").append(album_list);
+			}	
+		}
 
 	}
 });
