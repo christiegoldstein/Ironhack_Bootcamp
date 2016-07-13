afedf6f9613039751cc54e8362286bdc2d05217f
christiegoldstein <cngold15@gmail.com>
2016-07-13 17:51:20 -0400

    added modal to display list of tracks when clicking on link of album title

--- Week_5/Day_2/Spotify/app.js
+++ Week_5/Day_2/Spotify/app.js
@@ -17,23 +17,24 @@ $(document).on('ready',function(){
 	});
 
 	function showArtist(artist){
-		//artist.artists.items.length
 		$(".artist_info").empty();
-		for(var i = 0; i < 1; i++){
+		for(var i = 0; i < artist.artists.items.length; i++){
 			$(".artist_info").append("<h3>"+artist.artists.items[i].name+"</h3>");
-			$(".artist_info").append(`<a href='#' data=${artist.artists.items[i].id}>Click here to see albums</a>`);
+			var id = artist.artists.items[i].id;
+			//console.log(id);
+			$(".artist_info").append(`<a href='#' data=${id} class='album_link'>Click here to see albums</a>`);
+			//console.log($("a").attr("data"));
 			if(artist.artists.items[i].images[0] != null){
 				$(".artist_info").append('<img src='+artist.artists.items[i].images[1].url+'>');
 			}
 		}
 
-		$("a").on("click", function(event){
+		$(".album_link").on("click", function(event){
+			//console.log($("a").attr("data"));
 			event.preventDefault();
-		    $(".modal").modal("show");
+		    $(".albums_modal").modal("show");
 
-			var id = artist.artists.items[0].id;
-
-			console.log(id);
+			var id = $(event.currentTarget).attr("data");
 
 			var album_link = 'https://api.spotify.com/v1/artists/'+id+'/albums';
 
@@ -47,24 +48,60 @@ $(document).on('ready',function(){
 	}
 
 	function showAlbum(albums){
-		$(".modal-body").empty();
+		$(".body_album").empty();
 		for(var i = 0; i < albums.items.length; i++){
 			if(albums.items[i].name != null && albums.items[i].images[1].url != null){
 				var album_name = albums.items[i].name;
 				var album_img = albums.items[i].images[1].url;
-				console.log(album_name);
-				console.log(album_img);
+				var album_id = albums.items[i].id;
+				//console.log(album_name);
+				//console.log(album_img);
 				var album_list = `
 					<ul>
 						<li>	
-							<h4>${album_name}</h4>
+							<h4><a href='#' data=${album_id} class='track_link'>${album_name}<a></h4>
 							<img src=${album_img}>
 						</li>
 					</ul>
 				`;
-				$(".modal-body").append(album_list);
+				$(".body_album").append(album_list);
 			}	
 		}
 
+		$(".track_link").on("click", function(event){
+			//console.log($("a").attr("data"));
+			event.preventDefault();
+		    $(".tracks_modal").modal("show");
+
+			var id = $(event.currentTarget).attr("data");
+
+			var track_link = 'https://api.spotify.com/v1/albums/'+id+'/tracks';
+
+			 $.ajax({
+			 	type: "GET",
+			 	url: track_link,
+			 	success: showTrack
+			 });
+		});
+
+	}
+
+	function showTrack(tracks){
+		$(".body_track").empty();
+		for(var i = 0; i < tracks.items.length; i++){
+			if(tracks.items[i].name != null){
+				var track_name = tracks.items[i].name;
+				console.log(track_name);
+				var track_id = tracks.items[i].id;
+				var track_list = `
+					<ul>
+						<li>	
+							<h4>${track_name}</h4>
+						</li>
+					</ul>
+				`;
+				$(".body_track").append(track_list);
+			}	
+		}
 	}
 });
