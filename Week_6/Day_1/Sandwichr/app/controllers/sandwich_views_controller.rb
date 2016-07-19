890fe9e1cfe5cc1a1536bce63511e17a397a98ad
christiegoldstein <cngold15@gmail.com>
2016-07-19 14:18:49 -0400

    using yesterdays informatin as api for todays work with sandwich views

---
+++ Week_6/Day_1/Sandwichr/app/controllers/sandwich_views_controller.rb
@@ -0,0 +1,5 @@
+class SandwichViewsController < ApplicationController
+	def index
+		@sandwiches = Sandwich.all 
+	end
+end
--- Week_6/Day_1/Sandwichr/config/routes.rb
+++ Week_6/Day_1/Sandwichr/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :ingredients, only: [:create, :destroy, :show]
-  resources :sandwiches, except: [:new, :edit]
 
-  post "/sandwiches/:id/ingredients/add" => "sandwiches#createIngredient"
-
-
+	resources :sandwiches, only: [:index, :show], controller: "sandwich_views"
+  	scope "/api" do
+    	resources :ingredients, only: [:create, :destroy, :show]
+  		resources :sandwiches, except: [:new, :edit]
+    	post "/sandwiches/:id/ingredients/add" => "sandwiches#add_ingredient"
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
